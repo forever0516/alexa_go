63f165b45544e58b20269974477d572a1f84b57c
Kai Yang <Kai_Yang@wistron.com>
2017-05-03 18:31:31 +0800

    123

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.dictionary = {
   "contents":[]
 };
 
-var templateSubject='';
+var templateSubject='',
+    templateContent ='';
 
 app.launch(function(request, response) {
   response.say("welcome to gina .. what can I do for you").reprompt("welcome to gina mail").shouldEndSession(false);
@@ -119,184 +120,175 @@ app.launch(function(request, response) {
 // );
 
 //mail serverice
-app.intent("mailIntent", {
-    "slots": {
-
-    },
-    "utterances": ["mail serverice"]
-  },
-  function(request, response) {
-      console.log(JSON.stringify(request));
-      response.say("ok, mail service .. which feature do you want .. send mail .. check mail").reprompt("please say again").shouldEndSession(false);
-      // return false;
-  }
-);
+// app.intent("mailIntent", {
+//     "slots": {
+//
+//     },
+//     "utterances": ["mail serverice"]
+//   },
+//   function(request, response) {
+//       console.log(JSON.stringify(request));
+//       response.say("ok, mail service .. which feature do you want .. send mail .. check mail").reprompt("please say again").shouldEndSession(false);
+//       // return false;
+//   }
+// );
 
 //send mail & ask subject
-app.intent("sendMailIntent", {
-    "slots": {
-
-    },
-    "utterances": ["send mail"]
-  },
+app.intent("sendMailIntent",
+// {
+//     "slots": {
+//
+//     },
+//     // "utterances": ["send mail"]
+//   },
   function(request, response) {
       console.log(JSON.stringify(request));
+      templateSubject = '';
       response.say("ok, send mail .. what is your title").reprompt("please say again").shouldEndSession(false);
       // return false;
   }
 );
 
 // send mail get subject and ask content
-app.intent("mailSubjectIntent", {
-    "slots": {
-      "SUBJECT": "MailSubject"
-    },
-    "utterances": ["mail title {titles|SUBJECT} ", "title is {titles|SUBJECT}", "title name {titles|SUBJECT}"]
-
-  },
-  function(request, response) {
-      console.log(JSON.stringify(request));
-      response.say("ok, your mail title is " + request.slot("SUBJECT") + "..");
-      response.say("and what is your content ..").reprompt("please say again").shouldEndSession(false);
-      templateSubject = request.slot("SUBJECT");
-      // return false;
-  }
-);
+// app.intent("mailSubjectIntent", {
+//     "slots": {
+//       "SUBJECT": "MailSubject"
+//     },
+//     "utterances": ["mail title {titles|SUBJECT} ", "title is {titles|SUBJECT}", "title name {titles|SUBJECT}"]
+//
+//   },
+//   function(request, response) {
+//       console.log(JSON.stringify(request));
+//       response.say("ok, your mail title is " + request.slot("SUBJECT") + "..");
+//       response.say("and what is your content ..").reprompt("please say again").shouldEndSession(false);
+//       templateSubject = request.slot("SUBJECT");
+//       // return false;
+//   }
+// );
 
 //mail get content and send
-app.intent("mailContentIntent", {
-    "slots": {
-      "CONTENT": "MailContent"
-    },
-    "utterances": ["mail message {contents|CONTENT}" , "content {contents|CONTENT}", "message text {contents|CONTENT}"]
-  },
-  function(request, response) {
-
-    var session = request.getSession();
-    console.log('session: '+JSON.stringify(session));
-    var accessToken = session.details.accessToken;
-    if(accessToken){
-        // console.log('accessToken: ' + accessToken);
-        var client = MicrosoftGraph.Client.init({
-              authProvider: (done) => {
-                  done(null, accessToken);
-              }
-        });
-        //
-        var url = '/me/sendMail';
-        var replyMessage = 'Send an email';
-        //
-        var mail = {
-            subject: templateSubject,
-            toRecipients: [{
-                emailAddress: {
-                    address: "Kai_Yang@wistron.com"
-                }
-            }],
-            body: {
-                content: request.slot("CONTENT"),
-                contentType: "html"
-            }
-        }
-      client
-          .api('/me/sendMail')
-          .post(
-              {message: mail},
-              (err, res) => {
-                  if (err){
-                      console.log(err);
-                    }else{
-                      console.log('request content' + JSON.stringify(request) );
-                      console.log('res content' + JSON.stringify(res) );
-                      console.log('response content' + JSON.stringify(response) );
-                    }
-              })
-
-    response.say("send an mail title: "+ templateSubject +' now').reprompt("please say again").shouldEndSession(false);
-
-    }else{
-        console.log('no token');
-    }
-    return false;
-  }
-
-
-);
+// app.intent("mailContentIntent", {
+//     "slots": {
+//       "CONTENT": "MailContent"
+//     },
+//     "utterances": ["mail message {contents|CONTENT}" , "content {contents|CONTENT}", "message text {contents|CONTENT}"]
+//   },
+//   function(request, response) {
+//
+//     var session = request.getSession();
+//     console.log('session: '+JSON.stringify(session));
+//     var accessToken = session.details.accessToken;
+//     if(accessToken){
+//         // console.log('accessToken: ' + accessToken);
+//         var client = MicrosoftGraph.Client.init({
+//               authProvider: (done) => {
+//                   done(null, accessToken);
+//               }
+//         });
+//         //
+//         var url = '/me/sendMail';
+//         var replyMessage = 'Send an email';
+//         templateContent = request.slot("CONTENT");
+//         //
+//         var mail = {
+//             subject: templateSubject,
+//             toRecipients: [{
+//                 emailAddress: {
+//                     address: "Kai_Yang@wistron.com"
+//                 }
+//             }],
+//             body: {
+//                 content: templateContent,
+//                 contentType: "html"
+//             }
+//         }
+//       return client
+//           .api('/me/sendMail')
+//           .post({message:mail})
+//           .then((res) => {
+//             console.log('request content' + JSON.stringify(request) );
+//             console.log('res content' + JSON.stringify(res) );
+//             console.log('response content' + JSON.stringify(response) );
+//             response.say("send an mail title: "+ templateSubject +' now content: ' + templateContent).reprompt("please say again").shouldEndSession(false);
+//             templateSubject = '';
+//             templateContent = ''
+// ;          }).catch((err) => {
+//             console.log(err);
+//           });
+//
+//     }else{
+//         console.log('no token');
+//     }
+//     return false;
+//   });
 
 // check mail
-app.intent("checkMailIntent", {
-    "slots": {
-
-    },
-    "utterances": ["check mail"]
-
-  },
-  function(request, response) {
-
-      var session = request.getSession();
-      console.log('session: '+JSON.stringify(session));
-      var accessToken = session.details.accessToken;
-      if(accessToken){
-          // console.log('accessToken: ' + accessToken);
-          var client = MicrosoftGraph.Client.init({
-                authProvider: (done) => {
-                    done(null, accessToken);
-                }
-          });
-          //
-          var url = '/me/mailFolders/';
-          //
-                  client
-                  .api(url)
-                  .header("Prefer", 'outlook.timezone="Asia/Taipei"')
-                  .top(20)
-                  .get((err, res) => {
-                      if (err) {
-                          console.log(err)
-                          return;
-                      }else{
-                          console.log(url);
-                          console.log("check mail" + JSON.stringify(res));
-
-                          var upcomingEventNames = {
-                            displayName:'',
-                            unreadItemCount:'',
-                            totalItemCount:''
-                          };
-                          var str = "收件匣";
-                          for (var i=0; i<res.value.length; i++) {
-                              if(res.value[i].displayName == str){
-                                upcomingEventNames.displayName = res.value[i].displayName;
-                                upcomingEventNames.unreadItemCount = res.value[i].unreadItemCount;
-                                upcomingEventNames.totalItemCount = res.value[i].totalItemCount;
-                                console.log(res.value[i].displayName);
-                              }
-                            }
-
-
-                          console.log("mail box: " + JSON.stringify(upcomingEventNames));
-                          console.log("mail box JSON: " + JSON.stringify(upcomingEventNames.displayName));
-                          console.log("mail box: " + typeof(upcomingEventNames));
-                          console.log("mail Name: " + upcomingEventNames.displayName);
-                          console.log("mail unread: " + typeof(upcomingEventNames.unreadItemCount));
-                          console.log("mail total: " + typeof(upcomingEventNames.totalItemCount));
-                          // var replyMessage = 'you have '+ upcomingEventNames.length +' meeting today. . ';
-                          //
-                          // for(var i=1; i<=upcomingEventNames.length; i++){
-                          //     replyMessage += i+'. ' + res.value[i-1].subject + ' at ' + res.value[i-1].start.dateTime.substring(res.value[i-1].start.dateTime.lastIndexOf("T")+1,res.value[i-1].start.dateTime.lastIndexOf("."))+'. . ';
-                          // }
-
-                      }
-                  })
-
-      response.say("check mail: ").reprompt("please say again").shouldEndSession(false);
-
-      }else{
-          console.log('no token');
-      }
-      return false;
-    }
-);
+// app.intent("checkMailIntent", {
+//     "slots": {
+//
+//     },
+//     "utterances": ["check mail"]
+//
+//   },
+//   function(request, response) {
+//
+//       var session = request.getSession();
+//       console.log('session: '+JSON.stringify(session));
+//       var accessToken = session.details.accessToken;
+//       if(accessToken){
+//           // console.log('accessToken: ' + accessToken);
+//           var client = MicrosoftGraph.Client.init({
+//                 authProvider: (done) => {
+//                     done(null, accessToken);
+//                 }
+//           });
+//           //
+//           var url = '/me/mailFolders/';
+//           //
+//           return   client
+//                   .api(url)
+//                   .header("Prefer", 'outlook.timezone="Asia/Taipei"')
+//                   .top(20)
+//                   .get()
+//                   .then((res) => {
+//
+//                     console.log(url);
+//                     console.log("check mail" + JSON.stringify(res));
+//
+//                     var upcomingEventNames = {
+//                       displayName:'',
+//                       unreadItemCount:'',
+//                       totalItemCount:''
+//                     };
+//                     var replyMessage = 'test';
+//                     var str = "收件匣";
+//                     for (var i=0; i<res.value.length; i++) {
+//                         if(res.value[i].displayName == str){
+//                           upcomingEventNames.displayName = res.value[i].displayName;
+//                           upcomingEventNames.unreadItemCount = res.value[i].unreadItemCount;
+//                           upcomingEventNames.totalItemCount = res.value[i].totalItemCount;
+//                           console.log(res.value[i].displayName);
+//                         }
+//                       }
+//
+//                     console.log("mail box: " + JSON.stringify(upcomingEventNames));
+//                     console.log("mail Name: " + upcomingEventNames.displayName);
+//                     console.log("mail unread: " + upcomingEventNames.unreadItemCount);
+//                     console.log("mail total: " + upcomingEventNames.totalItemCount);
+//
+//                     replyMessage = "Receiver folder have unread mail " + upcomingEventNames.unreadItemCount + " and total mail " + upcomingEventNames.totalItemCount;
+//                     response.say(replyMessage).shouldEndSession(false);
+//
+//                   }).catch((err) =>{
+//                     console.log(err);
+//                   });
+//
+//       }else{
+//           console.log('no token');
+//       }
+//       return false;
+//     }
+// );
 
 app.intent("errorIntent", function(request, response) {
   response.say("error please say new intent").shouldEndSession(false);
